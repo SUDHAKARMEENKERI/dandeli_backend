2ea742487af4b5fb57e2cf55c010e23304d9a408
sudhakarmeenkeri <sudhakarmeenkeri@gmail.com>
2021-08-15 22:26:44 +0530

    working on whitepage

--- src/main/java/com/dandeli/com/DandeliApplication.java
+++ src/main/java/com/dandeli/com/DandeliApplication.java
@@ -3,8 +3,10 @@ package com.dandeli.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.dandeli.controller"})
 public class DandeliApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
