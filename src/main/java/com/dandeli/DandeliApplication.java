510166461759301222c0d12c19cb8117cf0f74d0
sudhakarmeenkeri <sudhakarmeenkeri@gmail.com>
2021-08-15 21:38:00 +0530

    changed

--- src/main/java/com/dandeli/DandeliApplication.java
+++ src/main/java/com/dandeli/DandeliApplication.java
@@ -2,9 +2,10 @@ package com.dandeli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DandeliApplication {
+public class DandeliApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DandeliApplication.class, args);
